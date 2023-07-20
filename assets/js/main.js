eda241f8f41d6066283856126d2ef43b9dfe0730
Lucas Exequiel <arrejoria1994@gmail.com>
2023-07-19 23:11:01 -0300

    Update Assets

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  body.classList.add(currentTheme);
+} else {
+  body.classList.add('light');
+}
+
+themeToggle.addEventListener('click', () => {
+  if (body.classList.contains('light')) {
+    body.classList.replace('light', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    body.classList.replace('dark', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+});
