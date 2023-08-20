858b315aefb8fe6b6eb943058de24f7ee26c38f3
Lucas Exequiel <arrejoria1994@gmail.com>
2023-08-20 00:57:53 -0300

    Update Assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,34 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener("scroll", showBubble);
   handleAvatarMessage();
 });
+
+// Skills Section Scripts 
+
+// Selecciona los contenedores de tecnología
+const stackGroup = document.querySelectorAll('.stack__category');
+
+// Configura las animaciones iniciales
+stackGroup.forEach(container => {
+  gsap.set(container, { opacity: 0, y: 50 });
+});
+
+// Función para manejar el desplazamiento
+function handleScroll() {
+  stackGroup.forEach(container => {
+    const containerPosition = container.getBoundingClientRect().top;
+    const scrollPosition = window.innerHeight * 0.8;
+
+    if (containerPosition < scrollPosition) {
+      gsap.to(container, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' });
+    } else {
+      gsap.to(container, { opacity: 0, y: 50, duration: 0.5, ease: 'power2.out' });
+    }
+  });
+}
+
+// Asigna la función de manejo de desplazamiento al evento de scroll
+window.addEventListener('scroll', handleScroll);
+
+// Llama a la función una vez al cargar la página para mostrar elementos visibles
+handleScroll();
+
